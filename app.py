cf59d22e8b3cce0e056778f66e3ee8e9a08a7dbc
Milyor <millerarmando20@gmail.com>
2023-11-28 18:25:51 -0400

    app.py changes adding redirect and imports

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, redirect
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    redirect ("layout.html")
+    return render_template("layout.html")
