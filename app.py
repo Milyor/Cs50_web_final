3f0f833489ce494e4b85aa72108694b55e781d0d
Milyor <millerarmando20@gmail.com>
2023-11-29 16:08:35 -0400

    moved recommender fiel to templates

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("layout.html")
+    return render_template("recommender.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
