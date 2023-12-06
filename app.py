0e31c35f5d0adf758e27709ab76137c4d744f991
Milyor <millerarmando20@gmail.com>
2023-12-06 00:12:12 -0400

    updated api search, trying to store genre

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from igdb.wrapper import IGDBWrapper
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from sklearn.feature_extraction.text import TfidfVectorizer
 from werkzeug.security import check_password_hash, generate_password_hash
 import re
 import json
@@ -17,19 +18,43 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    genres = []
     if request.method == "POST":
         game_name = request.form.get("game")
-        byte_array = wrapper.api_request(
-            'games',
-            f'search "{game_name}"; fields id, name, release_dates.human; limit 5;'
-        )
+        if not game_name:
+            flash("Fill out the field with a game")
+            return render_template("recommender.html")
         
-        decoded_data = json.loads(byte_array.decode('utf-8'))
+        try: 
+            #search for the game
+            response = wrapper.api_request(
+                'games',
+                f'search "{game_name}"; fields name, id; limit 1;'
+            )
+            decoded_data = json.loads(response.decode('utf-8'))
+            
+            #check if any games were found
+            if decoded_data:
+                game_id = decoded_data[0]['id']
+                
+                genres_response = wrapper.api_request(
+                    'games',
+                    f'fields genres.name; where id = {game_id}; limit 5;'
+                )
+                genres_data = json.loads(genres_response.decode('utf-8'))
+                if genres_data and 'genres' in genres_data[0]:
+                    genres = genres_data[0]['genres']
+                
+                return render_template("recommender.html", decoded_data=decoded_data, game_id=game_id, genres = genres)
+            
+            else:
+                
+                flash(f"No results found for the game: {game_name}")
+                
+        except Exception as e:
+        # Handle API request errors
+            flash(f"Error: {str(e)}")
         
-        game_names = [item['name'] for item in decoded_data]
-        
-        return render_template("recommender.html",game_names=game_names)
-    
     return render_template("recommender.html")
 
 
